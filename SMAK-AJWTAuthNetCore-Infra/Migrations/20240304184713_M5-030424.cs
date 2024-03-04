b29dca0bfcc0bedc2dc43dc8b9d2d76a28e7a9bd
mudassarali.official <mudassarali.official@gmail.com>
2024-03-05 00:40:57 +0500

    2018- Application Migration

---
+++ SMAK-AJWTAuthNetCore-Infra/Migrations/20240304184713_M5-030424.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMAK_AJWTAuthNetCore_Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class M5030424 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
